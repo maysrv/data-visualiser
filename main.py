7108ecbc19c6c18990d260223877b1e91983e1f3
maysrv <srvmay@hotmail.com>
2022-05-31 21:10:59 +0530

    a

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
+
 import utils as utils
 import function as fn
-from streamlit_pandas_profiling import st_profile_report
 from sklearn.model_selection import train_test_split
 
 if 'cstate' not in st.session_state:
